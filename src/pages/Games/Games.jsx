100d8fbff1b62261f9896bc837d5b64ea77cf1d1
AndreasB09 <andreasb09.kodehode@gmail.com>
2024-08-24 21:39:10 +0200

    pages setup

---
+++ src/pages/Games/Games.jsx
@@ -0,0 +1,11 @@
+import styles from "./Games.module.css";
+
+function Games() {
+    return (
+        <div className={styles.pGames}>
+            <p>Games</p>
+        </div>
+    )
+}
+
+export default Games;
---
+++ src/pages/Shop/Shop.jsx
@@ -0,0 +1,11 @@
+import styles from "./Shop.module.css";
+
+function Shop() {
+    return (
+        <div className={styles.pShop}>
+            <p>Shop</p>
+        </div>
+    )
+}
+
+export default Shop;
